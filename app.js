67d919f16293093e1becaa8eba26e9cc38193b51
Avaz <elonarm21@gmail.com>
2024-03-01 01:35:13 +0500

    classlist bilan tanishuv

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 "use strict"
 
 document.addEventListener('DOMContentLoaded',()=>{
+const btns = document.querySelectorAll("button")
+    console.log(btns)
+    console.log(btns[0].classList)
+    console.log(btns[0].classList.item(0))
+    btns[0].classList.add("lime")
+    btns[0].classList.remove("red")
+    btns[0].classList.toggle("blue")
 
 })
